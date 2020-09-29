bb0cd5eba051b3fecd295220806de60a94f88ba7
Rizwan Shaikh <rshaikh@ces-ltd.com>
2020-09-30 01:49:45 +0530

    Added gitignore

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://root:root123@ds163700.mlab.com:63700/devsocialnetwork' ,
-  secretorkey:'this is secret key',
-  githubClientId: "271c1c2b5d7e51f633f4",
-  githubClientSecret: "fc8df0e90e0564ee33ff5f28601636dff1a3c9fc"
-}
